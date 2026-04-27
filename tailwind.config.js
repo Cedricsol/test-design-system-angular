84e7f35661cfef795855bcf546c86b038ea8d4d2
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-27 10:31:16 +0200

    add and update tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
-        primary: '#4f46e5',
-        secondary: '#64748b',
+        content: {
+          primary: '#4f46e5',
+          secondary: '#64748b',
+          disabled: '#94a3b8',
+        },
+        text: {
+          primary: colors.gray[100],
+          secondary: colors.gray[200],
+        },
+        border: colors.gray[200],
+        hover: {
+          primary: colors.gray[300],
+          secondary: colors.gray[400],
+        },
       },
     },
   },
