f1ed8afe3b924744dd57e45eb814be3ad2c0f254
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-27 10:53:41 +0200

    add a variant 'scrollable' to the table

--- projects/ui/src/lib/components/table/table.ts
+++ projects/ui/src/lib/components/table/table.ts
@@ -1,12 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-table',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './table.html',
   styleUrl: './table.css',
 })
 export class Table<T = any> {
   @Input() columns: { key: keyof T; label: string }[] = [];
   @Input() data: T[] = [];
+  @Input() variant: 'default' | 'scrollable' = 'default';
 }
